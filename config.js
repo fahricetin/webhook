a1171b3e2ac3cbab561917fdd70748230ef504ed
baradm <baradm@CBTMFC-1686.barikat.local>
2024-09-25 21:52:15 +0300

    update readme

--- config.js
+++ config.js
@@ -3,6 +3,7 @@ module.exports = {
   password: process.env.AUTH_PASSWORD || 'password',
   port: process.env.PORT || 3000
 };
+// for API key
 /*module.exports = {
   apiKey: process.env.API_KEY || 'mykeyfortest',
   port: process.env.PORT || 3000
